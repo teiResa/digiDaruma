e8e8077eb15a2d2b7834627f3530f45e6137575f
Theresa <49374955+teiResa@users.noreply.github.com>
2023-04-13 18:25:44 +0800

    js for skins
    - Working on JS to have users able to make mods
    - skins.css, using scss, contains user styling.
    - testing skins

--- assets/scripts2.js
+++ assets/scripts2.js
@@ -1,2 +1,17 @@
-/* onclick of the update button, check whick colour is selected, then update the doll's body to be that colour. */
+/* onclick of the update button, check whick colour is selected, then update the doll's body to be that colour.
 
+The update button (html, line 25)
+<button type="button" onclick="updateSelections()">update</button>
+
+The skins.scss file contains all of the information about the looks that are user-changable
+
+
+
+
+*/
+const skin = document.querySelector(".skin-color");
+// querySelector() is more modern and faster
+
+function buttonColor() {
+ skin.style.color = "blue";
+}
