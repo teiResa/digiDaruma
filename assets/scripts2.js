172a4ca21c06091861e64b131554f5a437672b29
Theresa <49374955+teiResa@users.noreply.github.com>
2023-04-13 21:24:12 +0800

    skins
    - toggleMenu note
    - decided to switch to jQuery for the form stuff. Sounded fun in the moment.

--- assets/scripts2.js
+++ assets/scripts2.js
@@ -9,9 +9,34 @@ The skins.scss file contains all of the information about the looks that are use
 
 
 */
+
+/*
 const skin = document.querySelector(".skin-color");
 // querySelector() is more modern and faster
 
-function buttonColor() {
+function updateColor() {
  skin.style.color = "blue";
 }
+*/
+
+// Remember to link to the jQuery CDN 
+
+jQuery(document).ready(function() {
+       //https://www.scratchcode.io/jquery-form-submit-with-examples/
+
+
+       /*
+ jQuery('#customisationMenu').submit(function(){
+  alert('successfully hahaha submitted form');
+
+  // Your other code goes here
+});
+*/
+ jQuery('#btnSubmit').click(function(e){
+    e.preventDefault();
+    jQuery('#customisationMenu').submit();
+    alert('successfully updated doll ');
+
+    // Your other code goes here
+ });
+});
