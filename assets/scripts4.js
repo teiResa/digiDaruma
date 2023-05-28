370c8a28ecfd3c0ebfd925b0c6a7ff35508c92ed
Theresa <49374955+teiResa@users.noreply.github.com>
2023-05-28 15:31:49 +0800

    Brow, Bindi, Nose, Mouth, Facial Hair
    They're ready for sprites
    
    - Expressed each their own defaults in the js

--- assets/scripts4.js
+++ assets/scripts4.js
@@ -11,9 +11,53 @@
 
 I have decided to up the changing CSS here because the project is so small. Just seems like the simple choice. */
 
-$(document).ready(function () {
+/* Using let variables to set defaults */
+var currSkin = document.getElementById('skin-color');
+   currSkin.style.backgroundColor = "red";
+var currTorso = document.getElementById('torso');
+   currTorso.classList.add('spritePos1', 'chestCorrect');
+var currChest = document.getElementById('chest');
+   currChest.style.backgroundColor = "#E39FF6";
+var currEarL = document.querySelector('#ear-left');
+var currEarR = document.querySelector('#ear-right');
+   currEarL.classList.add('earSprite1l');
+   currEarR.classList.add('earSprite1r');
+var currBrowL = document.querySelector('#brow-left');
+var currBrowR = document.querySelector('#brow-right');
+   currBrowL.classList.add('browSprite1l');
+   currBrowR.classList.add('browSprite1r');
+var currBindi = document.getElementById('bindi');
+   currBindi.classList.add('bindiStyle1');
+var currNose = document.getElementById('nose');
+   currNose.classList.add('noseStyle1');
+var currMouth = document.getElementById('mouth');
+   currMouth.classList.add('mouthStyle1');
+var currCheekL = document.querySelector('#FH-left');
+var currCheekR = document.querySelector('#FH-right'); 
+   currCheekL.classList.add('fhSprite1l');
+   currCheekL.classList.remove('fhSprite2l', 'fhSprite3l');
+   currCheekR.classList.add('fhSprite1r');
 
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function () { /* Skin */
         
  $( '#color1' ).on( 'click', function() { 
      $('#displayBodyColor').html( 'Body color1 is selected!' );
@@ -51,7 +95,7 @@ $( '#color3' ).on( 'click', function() {
   <div id="displayChestPattern"> </div>
 */
 
-$(document).ready(function () {
+$(document).ready(function () { /* Torso & Chest */
 
  //For now, the torso and chest patterns are just represented by shades of purple FINISHME
         
@@ -120,7 +164,7 @@ $( '#chestPatt2' ).on( 'click', function() {
 */
 
 
-$(document).ready(function () {
+$(document).ready(function () { /* Ears */
         
  $( '#earSet1' ).on( 'click', function() { 
      $('#displayEarShape').html( 'Ear earSet1 is selected!' );
@@ -176,13 +220,14 @@ $( '#earSet3' ).on( 'click', function() {
        <div id="displayBindi"> </div> 
 */
 
-$(document).ready(function () {
+
+$(document).ready(function () { /* Brows */
         
  $( '#browSet1' ).on( 'click', function() { 
      $('#displayBrows').html( 'brow browSet1 is selected!' );
 
-     const currBrowL = document.getElementById('#brow-left');
-     const currBrowR = document.getElementById('#brow-right');
+     const currBrowL = document.querySelector('#brow-left');
+     const currBrowR = document.querySelector('#brow-right');
 
      currBrowL.classList.add('browSprite1l');
      currBrowL.classList.remove('browSprite2l', 'browSprite3l');
@@ -193,8 +238,8 @@ $(document).ready(function () {
 $( '#browSet2' ).on( 'click', function() { 
  $('#displayBrows').html( 'brow browSet2 is selected!' );
 
- const currBrowL = document.getElementById('#brow-left');
- const currBrowR = document.getElementById('#brow-right');
+ const currBrowL = document.querySelector('#brow-left');
+ const currBrowR = document.querySelector('#brow-right');
 
  currBrowL.classList.add('browSprite2l');
  currBrowL.classList.remove('browSprite1l', 'browSprite3l');
@@ -202,5 +247,184 @@ $( '#browSet2' ).on( 'click', function() {
  currBrowR.classList.remove('browSprite1r', 'browSprite3r');
 }); 
 
+$( '#browSet3' ).on( 'click', function() { 
+ $('#displayBrows').html( 'brow browSet3 is selected!' );
+
+ const currBrowL = document.querySelector('#brow-left');
+ const currBrowR = document.querySelector('#brow-right');
+
+ currBrowL.classList.add('browSprite3l');
+ currBrowL.classList.remove('browSprite1l', 'browSprite2l');
+ currBrowR.classList.add('browSprite3r');
+ currBrowR.classList.remove('browSprite1r', 'browSprite2r');
+}); 
+
+
+});
+
+/* <div class="brow-centre spriteContainerTemp" id="bindi"></div>
+ */
+
+$(document).ready(function () { /* Bindi */
+        
+ $( '#bindiPatt1' ).on( 'click', function() { 
+     $('#displayBindi').html( 'bindi bindiPatt1 is selected!' );
+
+     const currBindi = document.getElementById('bindi');
+
+     currBindi.classList.add('bindiStyle1');
+     currBindi.classList.remove('bindiStyle2', 'bindiStyle3');
+}); 
+
+$( '#bindiPatt2' ).on( 'click', function() { 
+ $('#displayBindi').html( 'bindi bindiPatt2 is selected!' );
+
+ const currBindi = document.getElementById('bindi');
+
+ currBindi.classList.add('bindiStyle2');
+ currBindi.classList.remove('bindiStyle1', 'bindiStyle3');
+}); 
+
+$( '#bindiPatt3' ).on( 'click', function() { 
+ $('#displayBindi').html( 'bindi bindiPatt3 is selected!' );
+
+ const currBindi = document.getElementById('bindi');
+
+ currBindi.classList.add('bindiStyle3');
+ currBindi.classList.remove('bindiStyle1', 'bindiStyle2');
+}); 
+
+});
+
+
+/* Snout and Facial Hair
+
+<div class="snout">
+      <div class="facial-hair-left spriteContainerTemp" id="FH-left"></div>
+      <div class="breathers">
+       <div class="nose spriteContainerTemp" id="nose"></div>
+       <div class="mouth spriteContainerTemp" id="mouth"></div>
+      </div>
+      <div class="facial-hair-right spriteContainerTemp" id="FH-right"></div>
+     </div>
+
+  <div id="displayFacialHair"> </div>
+  <div id="displayNose"> </div>
+  <div id="displayMouth"> </div> 
+
+  <input type="radio" name="nose-style" id="nose1" value="nose1">
+      <label for="nose1"> Style 1</label><br>
+     <input type="radio" name="nose-style" id="nose2" value="nose2">
+
+  <input type="radio" name="mouth-style" id="mouth1" value="mouth1">
+      <label for="mouth1"> Style 1</label><br>
+
+      <input type="radio" name="fhair-style" id="fhair3" value="fhair3">
+      <label for="fhair3"> Style 3</label><br>
+
+      .noseStyle1
+      mouthStyle3
+      fhSprite3r
+*/
+
+$(document).ready(function () { /* Nose */
+        
+ $( '#nose1' ).on( 'click', function() { 
+     $('#displayNose').html( 'nose nose1 is selected!' );
+
+     const currNose = document.getElementById('nose');
+
+     currNose.classList.add('noseStyle1');
+     currNose.classList.remove('noseStyle2', 'noseStyle3');
+}); 
+
+$( '#nose2' ).on( 'click', function() { 
+ $('#displayNose').html( 'nose nose2 is selected!' );
+
+ const currNose = document.getElementById('nose');
+
+ currNose.classList.add('noseStyle2');
+ currNose.classList.remove('noseStyle1', 'noseStyle3');
+}); 
+
+$( '#nose3' ).on( 'click', function() { 
+ $('#displayNose').html( 'nose nose3 is selected!' );
+
+ const currNose = document.getElementById('nose');
+
+ currNose.classList.add('noseStyle3');
+ currNose.classList.remove('noseStyle1', 'noseStyle2');
+}); 
+
+});
+
+$(document).ready(function () { /* Mouth */
+        
+ $( '#mouth1' ).on( 'click', function() { 
+     $('#displayMouth').html( 'mouth mouth1 is selected!' );
+
+     const currMouth = document.getElementById('mouth');
+
+     currMouth.classList.add('mouthStyle1');
+     currMouth.classList.remove('mouthStyle2', 'mouthStyle3');
+}); 
+
+$( '#mouth2' ).on( 'click', function() { 
+ $('#displayMouth').html( 'mouth mouth2 is selected!' );
+
+ const currMouth = document.getElementById('mouth');
+
+ currMouth.classList.add('mouthStyle2');
+ currMouth.classList.remove('mouthStyle1', 'mouthStyle3');
+}); 
+
+$( '#mouth3' ).on( 'click', function() { 
+ $('#displayMouth').html( 'mouth mouth3 is selected!' );
+
+ const currMouth = document.getElementById('mouth');
+
+ currMouth.classList.add('mouthStyle3');
+ currMouth.classList.remove('mouthStyle1', 'mouthStyle2');
+}); 
+
+});
+
+$(document).ready(function () { /* Facial hair or cheeks */
+        
+ $( '#fhair1' ).on( 'click', function() { 
+     $('#displayFacialHair').html( 'Cheeks fhair1 is selected!' );
+
+     const currCheekL = document.querySelector('#FH-left');
+     const currCheekR = document.querySelector('#FH-right');
+    
+     currCheekL.classList.add('fhSprite1l');
+     currCheekL.classList.remove('fhSprite2l', 'fhSprite3l');
+     currCheekR.classList.add('fhSprite1r');
+     currCheekR.classList.remove('fhSprite2r', 'fhSprite3r');
+}); 
+
+$( '#fhair2' ).on( 'click', function() { 
+ $('#displayFacialHair').html( 'Cheeks fhair2 is selected!' );
+
+     const currCheekL = document.querySelector('#FH-left');
+     const currCheekR = document.querySelector('#FH-right');
+
+     currCheekL.classList.add('fhSprite2l');
+     currCheekL.classList.remove('fhSprite1l', 'fhSprite3l');
+     currCheekR.classList.add('fhSprite2r');
+     currCheekR.classList.remove('fhSprite1r', 'fhSprite3r');
+}); 
+
+$( '#fhair3' ).on( 'click', function() { 
+ $('#displayFacialHair').html( 'Cheeks fhair3 is selected!' );
+
+     const currCheekL = document.querySelector('#FH-left');
+     const currCheekR = document.querySelector('#FH-right');
+
+     currCheekL.classList.add('fhSprite3l');
+     currCheekL.classList.remove('fhSprite1l', 'fhSprite2l');
+     currCheekR.classList.add('fhSprite3r');
+     currCheekR.classList.remove('fhSprite1r', 'fhSprite2r');
+}); 
 
 });
